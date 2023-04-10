dc6a73c408074e4049f61345e3fd7a56ba9d9e22
Gilgames <balintkovacs11@gmail.com>
2023-04-10 15:36:01 +0200

    cleanup comments

--- texture.cpp
+++ texture.cpp
@@ -2,20 +2,23 @@
 
 Color::Color() : r(0), g(0), b(0), a(0) {}
 
-Color::Color(const Color& c) : r(c.r), g(c.g), b(c.b), a(c.a) {}
+Color::Color(const Color &c) : r(c.r), g(c.g), b(c.b), a(c.a) {}
 
-Color::Color(Uint32 pixel){
+Color::Color(Uint32 pixel)
+{
     r = pixel & 0xff;
-    g = (pixel>>8) & 0xff;
-    b = (pixel>>16) & 0xff;
-    a = (pixel>>24) & 0xff;
+    g = (pixel >> 8) & 0xff;
+    b = (pixel >> 16) & 0xff;
+    a = (pixel >> 24) & 0xff;
 }
 
-Uint32 Color::ToPixel(){
-    return (r<<24) | (g<<16)| (b<<8) | a;
+Uint32 Color::ToPixel()
+{
+    return (r << 24) | (g << 16) | (b << 8) | a;
 }
 
-Texture::Texture(const char* fileName){
+Texture::Texture(const char *fileName)
+{
     texture = IMG_Load(fileName);
     if (texture == nullptr)
     {
@@ -25,55 +28,33 @@ Texture::Texture(const char* fileName){
     height = texture->h;
 }
 
-Texture::Texture(const Texture& t) : width(t.width), height(t.height){
+Texture::Texture(const Texture &t) : width(t.width), height(t.height)
+{
     // nem másolódik csak a pointere;
     texture = t.texture;
 }
 
-Texture::~Texture(){
+Texture::~Texture()
+{
     // SDL_FreeSurface(texture);
 }
 
-Uint32 Texture::GetPixel(int x, int y){
-    /*
-    // egyelőre csak 4bájt (RGBA)/pixel képpel működik
-    if (texture->format->BytesPerPixel != 4)
-        throw "WPO0HL";
-    
-    // kezdő memóriacím
-    Uint32 *pixel = (Uint32 *) texture->pixels;
-    //eltolás sor * sorhosszal
-    pixel += y * texture->pitch;
-    //eltolás oszloppal
-    pixel += x;
+Uint32 Texture::GetPixel(int x, int y)
+{
+    // bitmélység
+    const Uint8 bSize = texture->format->BytesPerPixel;
+    // addot pixel színe
+    Uint8 *pixel = (Uint8 *)texture->pixels + y * texture->pitch + x * bSize;
+    Uint32 pixelColor = *(Uint32 *)pixel;
+    Color c;
 
-    return *pixel;    
-    
-    
-    */
-    // Bytes per pixel
-    const Uint8 Bpp = texture->format->BytesPerPixel;
+    // this is a terrible way of doing this
+    SDL_GetRGBA(pixelColor, texture->format, &c.r, &c.g, &c.b, &c.a);
 
-    /*
-    Retrieve the address to a specific pixel
-    pSurface->pixels  = an array containing the SDL_Surface' pixels
-    pSurface->pitch       = the length of a row of pixels (in bytes)
-    X and Y               = the offset on where on the image to retrieve the pixel; (0, 0) is the upper left corner
-    */
-    Uint8* pPixel = (Uint8*)texture->pixels + y * texture->pitch + x * Bpp;
-
-    Uint32 PixelData = *(Uint32*)pPixel;
-    
-
-    Color pixelColor;
-    // Retrieve the RGB values of the specific pixel
-    SDL_GetRGBA(PixelData, texture->format, &pixelColor.r, &pixelColor.g, &pixelColor.b, &pixelColor.a);
-
-    return pixelColor.ToPixel();
-
+    return c.ToPixel();
 }
 
-Color Texture::GetColor(int x, int y){
+Color Texture::GetColor(int x, int y)
+{
     return Color(GetPixel(x, y));
 }
-
